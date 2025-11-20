63cb6e94917165376a9c4ce748d7188232e3f2f7
teemu, jalava <ag2353@student.jamk.fi>
2025-11-20 14:18:07 +0200

    apptestin pohja luotu

---
+++ tests/app.test.js
@@ -0,0 +1,8 @@
+import { describe, test, expect, it } from 'vitest';
+import laskin from '../public/app.js';
+/*describe-funktio ryhmittelee testit "Test Suiteksi". */
+describe('Taskin luonnin testaus', function () {
+  it('should do stuff', function () {});
+
+  test('it should do other stuff', function () {});
+});
